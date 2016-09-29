531bd174fa5cabf59b924fd538b5e8954858be16
OptimusCrime <thomasgautv@hotmail.com>
2016-09-29 14:11:36 +0200

    Naive attempt at predicting characters

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from preprocessing import Preprocessor
+import os
 
+from predicting import Predicter
+from preprocessing import Preprocessor
+from utilities import Config, Filesystem
 
 def main():
     # Do pre processing
-    preprocessor = Preprocessor()
-    preprocessor.run()
+    if Config.get('force') or not os.path.exists(Filesystem.get_root_path('data/data_set.pickl')):
+        preprocessor = Preprocessor()
+        preprocessor.run()
+
+    # Do the predicting
+    predicter = Predicter()
+    predicter.run()
 
 
 if __name__ == '__main__':
---
+++ src/predicting/predicter.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from utilities import Filesystem, pickle_data, unpickle_data
+
+class Predicter:
+
+    def __init__(self):
+        pass
+
+    def matrices_match(self, phrase, offset, character):
+        for i in range(len(character)):
+            if character[i] != phrase[offset + i]:
+                return False
+
+        return True
+
+    def naive_match(self, phrase, offset, character):
+        if len(phrase['matrix'][0]) - offset >= len(character['matrix'][0]):
+            # Do stuff
+            return self.matrices_match(phrase['matrix'][0], offset, character['matrix'][0])
+
+        return False
+
+    def run(self):
+        # Load the phrase
+        phrase = unpickle_data(Filesystem.get_root_path('data/phrase.pickl'))[0]
+
+        # Load the data set
+        data_set = unpickle_data(Filesystem.get_root_path('data/data_set.pickl'))
+
+        # WARNING: HEAVY WORK IN PROGRESS
+        phrase_length = len(phrase['matrix'][0])
+        predicts = []
+        for i in range(phrase_length):
+            for character_set in data_set:
+                if (self.naive_match(phrase, i, character_set)):
+                    predicts.append({
+                        'character': character_set['character'],
+                        'offset': i,
+                        'matrix': character_set['matrix'][0]
+                    })
+
+        print(phrase['matrix'][0])
+        print(predicts)
+
