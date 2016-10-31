57964a46ec37d3606d8717cfa9022e40dc741747
OptimusCrime <thomasgautv@hotmail.com>
2016-10-31 11:56:00 +0100

    Add sample cnn keras code

---
+++ src/conv/__init__.py
@@ -0,0 +1,1 @@
+from .predictor_wrapper import PredictorWrapper
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 
 import os
 
-from naive.predicting import PredictorWrapper
+from conv import PredictorWrapper
 from naive.wordbuilder import WordBuilder
 from preprocessing import Preprocessor
 from utilities import Config, Filesystem
@@ -16,9 +16,9 @@ def main():
     predictor = PredictorWrapper()
     predictor.run()
 
-    word_builder = WordBuilder()
-    word_builder.build()
-    word_builder.calculate(predictor.predictions)
+    #word_builder = WordBuilder()
+    #word_builder.build()
+    #word_builder.calculate(predictor.predictions)
 
 
 if __name__ == '__main__':
