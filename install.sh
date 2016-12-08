32e3855e008308133ce4ccac30c96f7052a5d796
OptimusCrime <thomasgautv@hotmail.com>
2016-12-08 13:24:46 +0100

    Add installation script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Copy config
+
+echo -e "\033[0;34mCopying config file\033[0m"
+cp config/config_example.yaml config/config.yaml
+
+# Fonts
+
+echo -e ""
+echo -e "\033[0;34mDownloading fonts\033[0m"
+echo -e ""
+curl -o fonts/arial.ttf https://raw.githubusercontent.com/OptimusCrime/master-thesis-resources/master/fonts/arial.ttf
+curl -o fonts/arial-mono.ttf https://raw.githubusercontent.com/OptimusCrime/master-thesis-resources/master/fonts/arial-mono.ttf
+
+# Wordlists
+
+echo -e ""
+echo -e "\033[0;34mDownloading wordlists\033[0m"
+echo -e ""
+curl -o config/wordlists//wordlist1.txt https://raw.githubusercontent.com/OptimusCrime/master-thesis-resources/master/wordlists/wordlist1.txt
+curl -o config/wordlists//wordlist2.txt https://raw.githubusercontent.com/OptimusCrime/master-thesis-resources/master/wordlists/wordlist2.txt
+curl -o config/wordlists//wordlist3.txt https://raw.githubusercontent.com/OptimusCrime/master-thesis-resources/master/wordlists/wordlist3.txt
+
+# Finish
+
+echo -e ""
+echo -e "\033[0;34mInstallation complete\033[0m"
