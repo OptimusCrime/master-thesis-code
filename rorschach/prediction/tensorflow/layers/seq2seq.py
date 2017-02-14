Swap validation and test naming, fix embedding calculation error

rorschach/prediction/tensorflow/layers/seq2seq.py
@@ -162,8 +162,8 @@ class Seq2Seq(object):
 
         return loss_v
 
-    def test_batch(self):
-        batchX, batchY = self.test_set.__next__()
+    def validate_batch(self):
+        batchX, batchY = self.validation_set.__next__()
 
         feed_dict = self.get_feed(
             batchX,
@@ -192,7 +192,7 @@ class Seq2Seq(object):
 
         return loss_v, accuracy, classification_correct, classification_total
 
-    def test(self):
+    def validate(self):
         losses = []
         accuracyies = []
 
@@ -203,7 +203,7 @@ class Seq2Seq(object):
         num_batches = int(math.ceil(Config.get('preprocessing.test-set.size') / Config.get('predicting.batch-size')))
 
         for i in range(num_batches):
-            loss_v, accuracy, correct_current, total_current = self.test_batch()
+            loss_v, accuracy, correct_current, total_current = self.validate_batch()
             losses.append(loss_v)
             accuracyies.append(accuracy)
 
@@ -212,7 +212,7 @@ class Seq2Seq(object):
 
         return np.mean(losses), np.mean(accuracyies), correct, total
 
-    def validate(self):
+    def test(self):
         pass
 
     def train(self):
@@ -235,13 +235,13 @@ class Seq2Seq(object):
 
             self.train_batch()
 
-            if epoch > 0 and (epoch + 1) % Config.get('predicting.test-interval') == 0:
+            if epoch > 0 and (epoch + 1) % Config.get('predicting.verify-interval') == 0:
                 # TODO
                 # save model to disk
                 # saver.save(sess, self.ckpt_path + self.model_name + '.ckpt', global_step=i)
 
                 # Test
-                test_loss, test_accuracy, test_correct, test_all = self.test()
+                test_loss, test_accuracy, test_correct, test_all = self.validate()
 
                 if self.callback is not None:
                     self.callback.run({
@@ -257,7 +257,7 @@ class Seq2Seq(object):
         self.log.write('', LogPrettifier.END)
 
         # Run final validation
-        self.validate()
+        self.test()
 
     def restore_last_session(self):
         # TODO

rorschach/transformation/handlers/finalize/swap_handler.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from rorschach.common import DataSetTypes
 from rorschach.transformation.handlers import BaseHandler
-from rorschach.utilities import Config
+from rorschach.utilities import Config, Filesystem, unpickle_data
 
 
 class SwapHandler(BaseHandler):
@@ -13,7 +13,19 @@ class SwapHandler(BaseHandler):
     def __init__(self):
         super().__init__()
 
+        self.voc_sizes = False
+
+    def prepare(self):
+        super().prepare()
+
+        self.data['voc_lower'] = 0
+        self.data['voc_upper'] = 0
+
     def run(self, input_lists):
+        super().run(input_lists)
+
+        self.calculate_voc_size()
+
         super().run(input_lists)
 
         return input_lists
@@ -21,6 +33,10 @@ class SwapHandler(BaseHandler):
     def list_handler(self, input_list, key):
         if key == DataSetTypes.LETTER_SET:
             return
+
+        if not self.voc_sizes:
+            self.count_voc_size(input_list)
+            return input_list
 
         inputs = SwapHandler.reshape_input(input_list)
         labels = SwapHandler.reshape_labels(input_list)
@@ -30,10 +46,13 @@ class SwapHandler(BaseHandler):
             DataSetTypes.LABELS: labels
         }
 
-        self.data['voc_size_labels'] = len(Config.get('general.characters')) + 2
-        self.data['voc_size_input'] = SwapHandler.calculate_voc_size(inputs)
-
         return new_list
+
+    def calculate_voc_size(self):
+        self.voc_sizes = True
+
+        self.data['voc_size_labels'] = len(unpickle_data(Config.get_path('path.data', 'labels.pickl')))
+        self.data['voc_size_input'] = self.data['voc_lower'] + self.data['voc_upper']
 
     @staticmethod
     def reshape_input(input_list):
@@ -51,16 +70,11 @@ class SwapHandler(BaseHandler):
 
         return np.array(raw_array)
 
-    @staticmethod
-    def calculate_voc_size(input_list):
-        voc_lower = 0
-        voc_upper = 0
+    def count_voc_size(self, input_list):
         for seq in input_list:
-            for val in seq:
-                if val < voc_lower:
-                    voc_lower = val
+            for val in seq[DataSetTypes.IMAGES]['input']:
+                if val < self.data['voc_lower']:
+                    self.data['voc_lower'] = val
 
-                if val > voc_upper:
-                    voc_upper = val
-
-        return abs(voc_lower) + voc_upper + 1
+                if val > self.data['voc_upper']:
+                    self.data['voc_upper'] = val