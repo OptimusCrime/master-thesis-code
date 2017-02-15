8e9707ed481a1759aac0f73b9d3d173954699d36
OptimusCrime <thomasgautv@hotmail.com>
2017-02-15 13:39:03 +0100

    Fix iterator bug

--- rorschach/prediction/tensorflow/tools/__init__.py
+++ rorschach/prediction/tensorflow/tools/__init__.py
@@ -1,3 +1,3 @@
-from .data_utils import *
+from .data_utils import batch_gen
 from .log_prettifier import LogPrettifier
 from .time_parse import TimeParse
--- rorschach/prediction/tensorflow/tools/data_utils.py
+++ rorschach/prediction/tensorflow/tools/data_utils.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from random import sample
-
-import numpy as np
-
-
-def split_dataset(x, y, ratio=[0.7, 0.15, 0.15]):
-    # number of examples
-    data_len = len(x)
-    lens = [int(data_len * item) for item in ratio]
-
-    trainX, trainY = x[:lens[0]], y[:lens[0]]
-    testX, testY = x[lens[0]:lens[0] + lens[1]], y[lens[0]:lens[0]+lens[1]]
-    validX, validY = x[-lens[-1]:], y[-lens[-1]:]
-
-    return (trainX, trainY), (testX, testY), (validX, validY)
-
 
 def batch_gen(x, y, batch_size):
+    if (len(x) % batch_size) != 0:
+        raise Exception('Batch must be dividable on batch_size')
+
     while True:
         for i in range(0, len(x), batch_size):
-            if (i + 1) * batch_size < len(x):
-                yield x[i:(i + 1) * batch_size].T, y[i:(i + 1) * batch_size].T
-
-
-def rand_batch_gen(x, y, batch_size):
-    while True:
-        sample_idx = sample(list(np.arange(len(x))), batch_size)
-        yield x[sample_idx].T, y[sample_idx].T
+            yield x[i:(i + batch_size)].T, y[i:(i + batch_size)].T
